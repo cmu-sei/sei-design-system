cba2ae999e0e1e79fc6510286b4acaeab817d4a4
Jason Shimkoski <jdshimkoski@sei.cmu.edu>
2022-02-22 14:15:51 -0500

    SDS-422 - improve Table

--- src/components/Table/Table.stories.js
+++ src/components/Table/Table.stories.js
@@ -20,13 +20,13 @@ const Template = (args) => ({
   },
   template: `
     <sds-table v-bind="args" class="table-prose">
-      <template #cell(title)="{ item, value }">
+      <template #cell(title)="{ item, value, format }">
         <p>{{ value }}</p>
         <p class="text-sm text-gray-500">
-          Created on {{ item.createdDate.toLocaleDateString() }}
+          ID: {{ item.id }} was created on {{ format('createdDate') }}
         </p>
       </template>
-      <template #actions-column="{ item }">
+      <template #cell(actions)="{ item }">
         <button @click="edit(item.id)">Edit</button>
       </template>
     </sds-table>
@@ -41,9 +41,11 @@ const Template = (args) => ({
 export const Default = Template.bind({});
 Default.args = {
   fields: [
-    { key: "id", label: 'ID', sortable: true },
+    { key: "id", label: 'ID', header: true },
     { key: "title", label: 'Title', sortable: true },
     { key: "lastDelivered", label: 'Last Delivered', sortable: true, format: (date) => date.toLocaleDateString() },
+    { key: "createdDate", label: 'Created Date', hidden: true, format: (date) => date.toLocaleDateString() },
+    { key: 'actions', label: 'Actions' }
   ],
   items: [
     { id: 1, title: "Apple", lastDelivered: new Date("01/01/2019"), createdDate: new Date("02/23/2009") },
@@ -53,7 +55,6 @@ Default.args = {
     { id: 5, title: "Elderberry", lastDelivered: new Date("01/01/2019"), createdDate: new Date("04/10/2017") },
   ],
   sortBy: 'lastDelivered',
-  sortDesc: true,
-  showActionsColumn: true
+  sortDesc: true
 };
 
