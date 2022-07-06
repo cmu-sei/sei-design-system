8355841e13643073e5e46cb9ad6ef3aba6aee39e
Jason Shimkoski <jdshimkoski@sei.cmu.edu>
2022-07-06 09:50:50 -0400

    SDS-568 SDS-569 SDS-570 SDS-571 - fix datepicker issues

---
+++ src/docs/components/DatepickerExample.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Example...</div>
+  <PropsTable :component="Component" />
+</template>
+
+<script setup lang="ts">
+import Component from "../../components/Datepicker/Datepicker.vue"
+import PropsTable from "./PropsTable.vue"
+</script>
