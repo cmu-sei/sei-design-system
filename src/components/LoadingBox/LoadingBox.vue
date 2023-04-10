52478019e8dc05a767ab47d27d4e50be6dfa2c0b
Jason Shimkoski <jdshimkoski@sei.cmu.edu>
2023-04-10 15:40:55 -0400

    fix loading box dark mode

--- src/components/LoadingBox/LoadingBox.vue
+++ src/components/LoadingBox/LoadingBox.vue
@@ -1,7 +1,7 @@
 <template>
   <span
     data-id="sds-loading-box"
-    :class="['loadingBox rounded flex flex-row relative overflow-hidden bg-gray-200', width, height]"
+    :class="['rounded flex flex-row relative overflow-hidden bg-gray-200 dark:bg-gray-800 animate-pulse', width, height]"
   />
 </template>
 
@@ -28,27 +28,3 @@ export default defineComponent({
   }
 })
 </script>
-
-<style scoped>
-.loadingBox::after {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  transform: translateX(-100%);
-  background: rgb(255,255,255);
-  /* Permalink - use to edit and share this gradient: https://colorzilla.com/gradient-editor/#dedfe0+33,efefef+50,dedfe0+66&0+0,1+33,1+50,1+66,0+100 */
-  background: -moz-linear-gradient(45deg,  rgba(222,223,224,0) 0%, rgba(222,223,224,1) 36%, rgba(239,239,239,1) 50%, rgba(222,223,224,1) 67%, rgba(222,223,224,0) 100%); /* FF3.6-15 */
-  background: -webkit-linear-gradient(45deg,  rgba(222,223,224,0) 0%,rgba(222,223,224,1) 36%,rgba(239,239,239,1) 50%,rgba(222,223,224,1) 67%,rgba(222,223,224,0) 100%); /* Chrome10-25,Safari5.1-6 */
-  background: linear-gradient(45deg,  rgba(222,223,224,0) 0%,rgba(222,223,224,1) 36%,rgba(239,239,239,1) 50%,rgba(222,223,224,1) 67%,rgba(222,223,224,0) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00dedfe0', endColorstr='#00dedfe0',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
-  animation: shimmer 6s infinite;
-  content: '';
-}
-@keyframes shimmer {
-  100% {
-    transform: translateX(100%);
-  }
-}
-</style>
