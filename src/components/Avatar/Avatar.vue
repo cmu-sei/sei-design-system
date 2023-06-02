fb15c14856d5020418a936655d7ebb691a4e1ca0
Matt R Winwood <mrwinwood@sei.cmu.edu>
2023-06-02 16:56:01 -0400

    adding aspect ratio for auto sized avatars

--- src/components/Avatar/Avatar.vue
+++ src/components/Avatar/Avatar.vue
@@ -104,9 +104,9 @@ const textClass = computed(() => {
 })
 const shapeClass = computed(() => {
   if (props.shape === 'circle') {
-    return 'rounded-full'
+    return 'rounded-full aspect-square'
   }
-  return ''
+  return 'aspect-[16/9]'
 })
 
 const variantClass = computed(() => {
