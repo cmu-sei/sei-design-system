ed391a4a4213ae09b898125bf4986f18acdfb414
Jason Shimkoski <jdshimkoski@sei.cmu.edu>
2023-06-26 13:38:13 -0400

    SDS-1003 - fix Avatar aspect ratio and add position prop

--- src/components/Avatar/Avatar.vue
+++ src/components/Avatar/Avatar.vue
@@ -6,7 +6,7 @@
     <div
       v-if="src"
       :title="name"
-      :class="['bg-cover bg-center', sizeClass, shapeClass]"
+      :class="['bg-cover', positionClass, sizeClass, shapeClass]"
       :style="`background-image: url(${src})`"
     />
     <span
@@ -31,28 +31,37 @@ import {computed, PropType} from 'vue'
 
 const props = defineProps({
   /**
-   * Determines the color of the component.
+   * Determines the background color of the avatar when no image is present.
    */
   variant: {
     type: String as PropType<'random' | 'gray' | 'red' | 'yellow' | 'green' | 'blue' | 'purple'>,
     default: 'gray'
   },
   /**
-   * Set the shape to a portrait or circle. Defaults to portrait.
+   * Determines the position of the image.
+   */
+  position: {
+    type: String as PropType<'bottom' | 'center' | 'left' | 'right' | 'top'>,
+    default: 'center'
+  },
+  /**
+   * Determines the shape of the avatar.
    */
   shape: {
     type: String as PropType<'portrait' | 'circle'>,
     default: 'portrait'
   },
   /**
-   * Set the size of the avatar.
+   * Determines the size of the avatar.
    */
   size: {
     type: String as PropType<'xs' | 'sm' | 'md' | 'lg' | 'auto'>,
     default: 'md'
   },
   /**
-   * Set the text name (ex. John Doe) will show "JD" initials as a placeholder when no image is present. The full name, "John Doe" will display on hover with or without an image present.
+   * Determines the text name (ex. John Doe) will show "JD" initials as a placeholder when no image is present.
+   * 
+   * The full name, "John Doe" will display on hover with or without an image present.
    */
   name: {
     type: String,
@@ -66,31 +75,46 @@ const props = defineProps({
     default: ''
   }
 })
+const positionClass = computed(() => {
+  switch(props.position) {
+    case 'bottom':
+      return 'bg-bottom'
+    case 'left':
+      return 'bg-left'
+    case 'right':
+      return 'bg-right'
+    case 'top':
+      return 'bg-top'
+    case 'center':
+    default:
+      return 'bg-center'
+  }
+})
 const sizeClass = computed(() => {
   if (props.shape === 'circle') {
     switch (props.size) {
       case 'lg':
-        return 'w-44 h-44'
+        return 'w-44'
       case 'md':
-        return 'w-16 h-16'
+        return 'w-16'
       case 'sm':
-        return 'w-12 h-12'
+        return 'w-12'
       case 'xs':
-        return 'w-8 h-8'
+        return 'w-8'
     }
   } else if (props.shape === 'portrait') {
     switch (props.size) {
       case 'lg':
-        return 'w-44 h-52'
+        return 'w-44'
       case 'md':
-        return 'w-16 h-20'
+        return 'w-16'
       case 'sm':
-        return 'w-10 h-12'
+        return 'w-10'
       case 'xs':
-        return 'w-6 h-8'
+        return 'w-6'
     }
   }
-  return 'w-full h-full'
+  return 'w-full'
 })
 const textClass = computed(() => {
   if (props.size === 'lg') {
@@ -107,7 +131,7 @@ const shapeClass = computed(() => {
   if (props.shape === 'circle') {
     return 'rounded-full aspect-square'
   }
-  return 'aspect-[16/9]'
+  return 'aspect-[4/5]'
 })
 
 const variantClass = computed(() => {
@@ -136,6 +160,4 @@ const initials = computed(() => {
   }
   return name.substring(0, 2)
 })
-
-
 </script>
