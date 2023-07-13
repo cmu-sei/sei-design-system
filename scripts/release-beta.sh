94d7a5f2cf4ca13c3279fc2272c9618cf1b84ce1
Jason Shimkoski <jdshimkoski@sei.cmu.edu>
2023-07-13 15:40:09 -0400

    SDS-1048 - update beta release script

--- scripts/release-beta.sh
+++ scripts/release-beta.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-# commit confirm prompt
-read -p "Have you committed all changes? (y,n) " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    exit 0
-fi
-
 # version type confirm prompt
 read -p 'Enter a version type (prerelease to bump beta version [recommended], or premajor, preminor, or prepatch for new prerelease): ' version
 
