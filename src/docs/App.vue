7f9858d4d5b8ad54cea6d5ec409942d4adcce449
Jason Shimkoski <jdshimkoski@sei.cmu.edu>
2023-08-07 11:02:05 -0400

    SDS-1087 - clean up panel, fix animation issue in modal and panel

--- src/docs/App.vue
+++ src/docs/App.vue
@@ -1,74 +1,89 @@
 <template>
   <sds-layout-app
-      v-model="collapsed"
-      :app-icon-url="appIconUrl"
-      :app-name="appName"
-      :app-suite="appSuite"
-      :app-url="appUrl"
-      :enable-collapsible-sidebar="enableCollapsibleSidebar"
-      :page-title="pageTitle"
-      :sidebar-navigation-items="sidebarNavigationItems"
-      @navigate="navigate"
+    v-model="collapsed"
+    :app-icon-url="appIconUrl"
+    :app-name="appName"
+    :app-suite="appSuite"
+    :app-url="appUrl"
+    :enable-collapsible-sidebar="enableCollapsibleSidebar"
+    :page-title="pageTitle"
+    :sidebar-navigation-items="sidebarNavigationItems"
+    @navigate="navigate"
   >
     <template #suite-header>
       Suite header content
     </template>
-    <sds-button variant="default"
-                @click="showPanel = !showPanel">Show Panel
+    <sds-button
+      variant="default"
+      @click="showPanel = !showPanel"
+    >
+      Show Panel
     </sds-button>
 
     {{ showPanel }}
 
-    <SdsPanel v-model="showPanel"
-              size="md"
+    <SdsPanel
+      v-model="showPanel"
     >
       <template #title>
-        Panel title
-      </template>
-      <template #navigationIcon>
-        <svg
-            class="w-6 h-6 cursor-pointer self-center mr-2"
+        <button
+          class="flex items-center gap-2"
+          @click="showPanel = false"
+        >
+          <svg
+            class="w-6 h-6"
             height="32"
             viewBox="0 0 24 24"
             width="32"
             xmlns="http://www.w3.org/2000/svg"
-            @click="showPanel = false"
-        >
-          <path d="m12 20l-8-8l8-8l1.425 1.4l-5.6 5.6H20v2H7.825l5.6 5.6L12 20Z"
-                fill="currentColor" />
-        </svg>
+          >
+            <path
+              d="m12 20l-8-8l8-8l1.425 1.4l-5.6 5.6H20v2H7.825l5.6 5.6L12 20Z"
+              fill="currentColor"
+            />
+          </svg>
+          <span>Panel title</span>
+        </button>
       </template>
       <div class="relative h-full overflow-hidden rounded-xl border border-dashed border-gray-400 opacity-75">
-        <svg class="absolute inset-0 h-full w-full stroke-gray-900/10"
-             fill="none">
+        <svg
+          class="absolute inset-0 h-full w-full stroke-gray-900/10"
+          fill="none"
+        >
           <defs>
-            <pattern id="pattern-510798f3-74a4-4150-a0cf-4e93e8f4fbdf"
-                     height="10"
-                     patternUnits="userSpaceOnUse"
-                     width="10"
-                     x="0"
-                     y="0">
-              <path d="M-3 13 15-5M-5 5l18-18M-1 21 17 3"></path>
+            <pattern
+              id="pattern-510798f3-74a4-4150-a0cf-4e93e8f4fbdf"
+              height="10"
+              patternUnits="userSpaceOnUse"
+              width="10"
+              x="0"
+              y="0"
+            >
+              <path d="M-3 13 15-5M-5 5l18-18M-1 21 17 3" />
             </pattern>
           </defs>
-          <rect fill="url(#pattern-510798f3-74a4-4150-a0cf-4e93e8f4fbdf)"
-                height="100%"
-                stroke="none"
-                width="100%"></rect>
+          <rect
+            fill="url(#pattern-510798f3-74a4-4150-a0cf-4e93e8f4fbdf)"
+            height="100%"
+            stroke="none"
+            width="100%"
+          />
         </svg>
       </div>
 
       <template #footer>
         <div class="space-x-2">
           <SdsButton
-              variant="primary"
-              @click="showPanel= false"
-          >Save
+            variant="primary"
+            @click="showPanel= false"
+          >
+            Save
           </SdsButton>
           <SdsButton
-              variant="default"
-              @click="showPanel = false"
-          >Cancel
+            variant="default"
+            @click="showPanel = false"
+          >
+            Cancel
           </SdsButton>
         </div>
       </template>
