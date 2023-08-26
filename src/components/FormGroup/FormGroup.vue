c985174c5d16f40d67f6cb9aa0ff5e7607c194b0
Jason Shimkoski <jdshimkoski@sei.cmu.edu>
2023-08-26 14:07:06 -0400

    allow Form Group to be fieldset, simplify Checkbox Group and Radio Group

--- src/components/FormGroup/FormGroup.vue
+++ src/components/FormGroup/FormGroup.vue
@@ -1,12 +1,15 @@
 <template>
-  <div
+  <Component
+    :is="el"
     ref="root"
     v-uid
+    :disabled="el === 'fieldset' ? disabled : undefined"
     data-id="sds-form-group"
     class="w-full"
   >
-    <label
-      :for="id"
+    <Component
+      :is="el === 'div' ? 'label' : 'legend'"
+      :for="el === 'div' ? id : undefined"
       :class="{
         'font-medium': labelWeight === 'medium',
         'font-semibold': labelWeight === 'semibold',
@@ -14,17 +17,36 @@
       }"
       class="flex gap-1 items-center mb-2"
     >
-      <slot name="label"><span>{{ label }}</span></slot>
+      <!-- @slot Label slot content. This will override the `label` prop. @binding label. -->
+      <slot
+        name="label"
+        :label="label"
+      >
+        <span>{{ label }}</span>
+      </slot>
       <span
         v-if="required"
         class="font-normal text-red-500 dark:text-red-300 text-xs"
       >* required</span>
-    </label>
-    <!-- @slot Default slot content. This is where you add the form field. @binding id, valid, invalid, required, readonly -->
+    </Component>
+    <p
+      v-if="descriptionPosition === 'top' && (description || $slots.description)"
+      class="block text-xs italic text-gray-600 dark:text-gray-500 pb-2"
+    >
+      <!-- @slot Description slot content. This will override the `description` prop. @binding description. -->
+      <slot
+        name="description"
+        :description="description"
+      >
+        <span>{{ description }}</span>
+      </slot>
+    </p>
+    <!-- @slot Default slot content. This is where you add the form field. @binding id, valid, invalid, disabled, required, readonly. -->
     <slot
       :id="id"
       :valid="state"
       :invalid="state === false"
+      :disabled="disabled"
       :required="required"
       :readonly="readonly"
     />
@@ -32,8 +54,11 @@
       v-if="state && (validFeedback || $slots.validFeedback)"
       class="block text-xs italic text-green-700 dark:text-green-300 py-1"
     >
-      <!-- @slot Valid Feedback slot content. This will override the `validFeedback` prop. -->
-      <slot name="validFeedback">
+      <!-- @slot Valid Feedback slot content. This will override the `validFeedback` prop. @binding validFeedback. -->
+      <slot
+        name="validFeedback"
+        :valid-feedback="validFeedback"
+      >
         <span>{{ validFeedback }}</span>
       </slot>
     </p>
@@ -41,21 +66,27 @@
       v-else-if="state === false && (invalidFeedback || $slots.invalidFeedback)"
       class="block text-xs italic text-red-500 dark:text-red-300 py-1"
     >
-      <!-- @slot Invalid Feedback slot content. This will override the `invalidFeedback` prop. -->
-      <slot name="invalidFeedback">
+      <!-- @slot Invalid Feedback slot content. This will override the `invalidFeedback` prop. @binding invalidFeedback. -->
+      <slot
+        name="invalidFeedback"
+        :invalid-feedback="invalidFeedback"
+      >
         <span>{{ invalidFeedback }}</span>
       </slot>
     </p>
     <p
-      v-else-if="description || $slots.description"
+      v-else-if="descriptionPosition === 'bottom' && (description || $slots.description)"
       class="block text-xs italic text-gray-600 dark:text-gray-500 py-1"
     >
-      <!-- @slot Description slot content. This will override the `description` prop. -->
-      <slot name="description">
+      <!-- @slot Description slot content. This will override the `description` prop. @binding description. -->
+      <slot
+        name="description"
+        :description="description"
+      >
         <span>{{ description }}</span>
       </slot>
     </p>
-  </div>
+  </Component>
 </template>
 
 <script lang="ts">
@@ -75,15 +106,20 @@ import { ref, computed, PropType } from 'vue'
 
 const props = defineProps({
   /**
+   * Determines whether to render the component as a fieldset with a legend or
+   * a div with a label.
+   */
+  el: { type: String as PropType<'div' | 'fieldset'>, default: 'div' },
+  /**
    * Determines the text of the label.
    */
-  label: { type: String, default: null },
+  label: { type: String as PropType<string | null>, default: null },
   /**
    * Optionally determine the value of the label's for attribute. Only
    * necessary if you aren't using the automatically generated
-   * id.
+   * id and `el` equals `div`.
    */
-  labelFor: { type: String, default: null },
+  labelFor: { type: String as PropType<string | null>, default: null },
   /**
    * Determines the font weight of the label.
    */
@@ -91,17 +127,21 @@ const props = defineProps({
   /**
    * Determines the description/helper text of the form field.
    */
-  description: { type: String, default: null },
+  description: { type: String as PropType<string | null>, default: null },
+  /**
+   * Determines the description/helper text of the form field.
+   */
+  descriptionPosition: { type: String as PropType<'top' | 'bottom'>, default: 'bottom' },
   /**
    * Determines the success message displayed when `state`
    * is `true`
    */
-  validFeedback: { type: String, default: null },
+  validFeedback: { type: String as PropType<string | null>, default: null },
   /**
    * Determines the failure message displayed when `state`
    * is `false`
    */
-  invalidFeedback: { type: String, default: null },
+  invalidFeedback: { type: String as PropType<string | null>, default: null },
   /**
    * Determines the valid/invalid state of the form group.
    * 
@@ -118,6 +158,10 @@ const props = defineProps({
    * Determines if the form group is readonly.
    */
   readonly: { type: Boolean, default: false },
+  /**
+   * Determines whether the form group is disabled or not.
+   */
+  disabled: { type: Boolean, default: false },
 })
 
 const root = ref()
