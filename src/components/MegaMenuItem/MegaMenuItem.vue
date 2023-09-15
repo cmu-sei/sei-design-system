96896522807d8043e50433a33b3596c8e752cef9
Daniel Tompkins <dwtompkins@sei.cmu.edu>
2023-09-15 15:48:37 -0400

    Updated dark-mode highlight color for red accents.

--- src/components/MegaMenuItem/MegaMenuItem.vue
+++ src/components/MegaMenuItem/MegaMenuItem.vue
@@ -14,9 +14,16 @@
       <div v-if="$slots.left">
         <slot name="left" />
       </div>
-      <div class="flex flex-col gap-1">
+      <div
+        :class="[
+          kind === 'descriptive'
+            ? 'gap-1'
+            : '',
+          'flex flex-col'
+        ]"
+      >
         <div class="inline-flex group">
-          <p class="group-hover:text-red-500">{{ label }}</p>
+          <p class="group-hover:text-red-500 dark:group-hover:text-red-300">{{ label }}</p>
           <svg
             v-if="cta || kind === 'landing-page'"
             class="w-4 h-4 ml-2 my-auto transition-all text-red-500 dark:text-red-300 group-hover:ml-4"
@@ -71,7 +78,7 @@ export default defineComponent({
     const linkClass = computed(() => {
       /* If the kind is set, or if `cta` prop is set, apply styles */
       let classes = props.kind || props.cta
-        ? 'link group w-full px-4 no-underline'
+        ? 'link group w-full no-underline'
         : ''
       return classes
     })
@@ -84,11 +91,11 @@ export default defineComponent({
         | "simple"
       switch (kind) {
         case "landing-page":
-          return "text-lg mb-8 hover:no-underline hover:text-red-500"
+          return "px-4 text-lg mb-8 hover:no-underline hover:text-red-500 dark:hover:text-red-300"
         case "descriptive":
           return "p-4 transition-all rounded-lg hover:no-underline hover:bg-gray-50 hover:dark:bg-gray-850"
         case "simple":
-          return "mb-2 p-4 text-sm transition-all rounded-lg hover:bg-gray-50 hover:dark:bg-gray-850 hover:text-red-500"
+          return "mb-2 p-4 text-sm transition-all rounded-lg hover:bg-gray-50 hover:dark:bg-gray-850 hover:text-red-500 dark:hover:text-red-300"
         default:
           return ""
       }
