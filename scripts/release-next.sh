7bb3838d5bb0076a52afa0f2accdb2de1515e2d9
Jason Shimkoski <jdshimkoski@sei.cmu.edu>
2023-11-08 10:01:24 -0500

    SDS-1261 - add release-next script, update package.json version

---
+++ scripts/release-next.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+# version type confirm prompt
+read -p 'Enter a version type (prerelease to bump next version [recommended], or premajor, preminor, or prepatch for new prerelease): ' version
+
+if [ "$version" == "premajor" ] || [ "$version" == "preminor" ] || [ "$version" == "prepatch" ] || [ "$version" == "prerelease" ] ;then
+    echo Proceeding with $version next release...
+
+    echo Building Storybook...
+    rm -rf storybook-static/
+    npm run build-storybook
+
+    echo Bundling components...
+    npm run bundle:release
+
+    echo Publishing...
+    npm version $version --preid=next
+    git push --follow-tags
+    npm publish --tag next --@sds:registry=https://artifacts.sei.cmu.edu/artifactory/api/npm/sei-design-system/
+    npm publish --tag next --@sds:registry=https://artifacts-new.sei.cmu.edu/artifactory/api/npm/sei-design-system/
+else
+    printf "\nCould not process release.\nEnsure to use a version type for this release: premajor, preminor, prepatch, preminor.\n\n"
+    exit 0
+fi
