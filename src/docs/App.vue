e93d82a4080649aa8b230debda841103d7a4ce96
Matt R Winwood <mrwinwood@sei.cmu.edu>
2024-03-04 14:46:00 -0500

    updating action bar styles for dark mode SDS-1433

--- src/docs/App.vue
+++ src/docs/App.vue
@@ -71,6 +71,7 @@
         <span class="sr-only">Toggle dark mode</span>
       </sds-button>
     </template>
+    <template #actions-bar>hello</template>
     <template #default>
       <RouterView />
     </template>
