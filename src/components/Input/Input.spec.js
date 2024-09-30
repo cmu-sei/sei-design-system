a086940f2daeb8699b8b7dacba59fbb0e9e0273b
Sean Stacho <sstacho@sei.cmu.edu>
2024-09-30 12:02:45 -0400

    SDS-1680 - add unit tests for "maxlength", "countCharacters", and "id" props

--- src/components/Input/Input.spec.js
+++ src/components/Input/Input.spec.js
@@ -8,7 +8,35 @@ describe('Input', () => {
     expect(wrapper.element).toMatchSnapshot()
   })
 
-  it('matches snapshot with placeholder prop assigned', () => {
+  it('should match its snapshot with maxlength prop assigned', () => {
+    const wrapper = shallowMount(Component, {
+      props: {
+        maxlength: 200
+      }
+    })
+    expect(wrapper.html()).toMatchSnapshot();
+  })
+
+  it('should match its snapshot with countCharacters prop assigned', () => {
+    const wrapper = shallowMount(Component, {
+      props: {
+        countCharacters: true,
+        type: 'number'
+      }
+    })
+    expect(wrapper.html()).toMatchSnapshot();
+  })
+
+  it('should match its snapshot with id prop assigned', () => {
+    const wrapper = shallowMount(Component, {
+      props: {
+        id: 'unique-identifier'
+      }
+    })
+    expect(wrapper.html()).toMatchSnapshot();
+  })
+
+  it('should match its snapshot with placeholder prop assigned', () => {
     const wrapper = shallowMount(Component, {
       props: {
         placeholder: 'Firstname Lastname'
