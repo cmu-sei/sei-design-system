262ea604c46277d5d0a2fdde68e1dda33b4ccbba
Sean Stacho <sstacho@sei.cmu.edu>
2024-10-03 16:24:11 -0400

    SDS-1638 - address dev feedback

---
+++ sds-types.d.ts
@@ -0,0 +1,84 @@
+import type DropdownPlacement from './src/components/Dropdown'
+import type { CalendarDate, CalendarMode, CalendarRange } from './src/components/Calendar/Calendar.vue'
+import type { CheckboxGroupOption, CheckboxGroupOptionValue } from './src/components/CheckboxGroup/CheckboxGroup.vue'
+import type { ComboBoxSuggestion } from './src/components/ComboBox/ComboBox.vue'
+import type { DatepickerPlacement } from './src/components/Datepicker/Datepicker.vue'
+import type { FileWithInvalidDefinitions } from './src/components/FileUploader/FileUploader.vue'
+import type { FilterByDropdownOption, FilterByDropdownPlacement } from './src/components/FilterByDropdown/FilterByDropdown.vue'
+import type { FloatingUiPlacement } from './src/components/FloatingUi/FloatingUi.vue'
+import type { LayoutAppSidebarNavItem } from './src/components/LayoutApp/LayoutApp.vue'
+import type { MegaMenuItem } from './src/components/MegaMenu/MegaMenu.vue'
+import type { MobileMenuItem } from './src/components/MobileMenu/MobileMenu.vue'
+import type { MultiselectOption, MultiselectTag } from './src/components/Multiselect/Multiselect.vue'
+import type { PopoverPlacement } from './src/components/Popover/Popover.vue'
+import type { RadioGroupOption, RadioGroupOptionValue } from './src/components/RadioGroup/RadioGroup.vue'
+import type { SelectOption, SelectOptionValue } from './src/components/Select/Select.vue'
+import type { TableField, TableItem } from './src/components/Table/Table.vue'
+import type { TabItem } from './src/components/Tabs/Tabs.vue'
+import type { ToasterToast } from './src/components/Toaster/Toaster.vue'
+import type { TooltipPlacement } from './src/components/Tooltip/Tooltip.vue'
+import type { TopFiveChartResult } from './src/components/TopFiveChart/TopFiveChart.vue'
+
+export type {
+  DropdownPlacement,
+  CalendarDate,
+  CalendarMode,
+  CalendarRange,
+  CheckboxGroupOption,
+  CheckboxGroupOptionValue,
+  ComboBoxSuggestion,
+  DatepickerPlacement,
+  FileWithInvalidDefinitions,
+  FilterByDropdownOption,
+  FilterByDropdownPlacement,
+  FloatingUiPlacement,
+  LayoutAppSidebarNavItem,
+  MegaMenuItem,
+  MobileMenuItem,
+  MultiselectOption,
+  MultiselectTag,
+  PopoverPlacement,
+  RadioGroupOption,
+  RadioGroupOptionValue,
+  SelectOption,
+  SelectOptionValue,
+  TableField,
+  TableItem,
+  TabItem,
+  ToasterToast,
+  TooltipPlacement,
+  TopFiveChartResult
+}
+
+export namespace SDS {
+  export {
+    DropdownPlacement,
+    CalendarDate,
+    CalendarMode,
+    CalendarRange,
+    CheckboxGroupOption,
+    CheckboxGroupOptionValue,
+    ComboBoxSuggestion,
+    DatepickerPlacement,
+    FileWithInvalidDefinitions,
+    FilterByDropdownOption,
+    FilterByDropdownPlacement,
+    FloatingUiPlacement,
+    LayoutAppSidebarNavItem,
+    MegaMenuItem,
+    MobileMenuItem,
+    MultiselectOption,
+    MultiselectTag,
+    PopoverPlacement,
+    RadioGroupOption,
+    RadioGroupOptionValue,
+    SelectOption,
+    SelectOptionValue,
+    TableField,
+    TableItem,
+    TabItem,
+    ToasterToast,
+    TooltipPlacement,
+    TopFiveChartResult
+  }
+}
