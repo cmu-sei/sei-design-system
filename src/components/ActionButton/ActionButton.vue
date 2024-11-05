eb753e724b9ee1c7d3c8b87d24edaa17c7e71c32
Sean Stacho <sstacho@sei.cmu.edu>
2024-11-05 14:28:44 -0500

    SDS-1674 - address issue with previous merge update

--- src/components/ActionButton/ActionButton.vue
+++ src/components/ActionButton/ActionButton.vue
@@ -12,8 +12,60 @@
     :aria-disabled="disabled"
     @click="onClick"
   >
-    <!-- @slot ActionButton content. -->
-    <slot />
+    <span
+      v-if="pending"
+      class="relative flex items-center"
+    >
+      <span
+        class="absolute inset-0 flex items-center"
+        :class="{
+          'justify-center': !block
+        }"
+      >
+        <svg
+          class="animate-spin text-current"
+          :class="{
+            'h-5 w-5': size === 'lg',
+            'h-4 w-4': size !== 'lg' && size !== 'xs' && size !== 'sm',
+            'h-3.5 w-3.5': size === 'sm',
+            'h-3 w-3': size === 'xs'
+          }"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          role="graphics-symbol"
+        >
+          <title>Pending</title>
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          />
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          />
+        </svg>
+      </span>
+      <span
+        class="inline-flex items-center opacity-0"
+        :class="{
+          'gap-2': size !== 'xs',
+          'gap-1': size === 'xs'
+        }"
+      >
+        <!-- @slot ActionButton content. -->
+        <slot />
+      </span>
+    </span>
+    <template v-else>
+      <!-- @slot ActionButton content. -->
+      <slot />
+    </template>
   </component>
 </template>
 
