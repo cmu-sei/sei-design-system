3db9f3e07b98dc0ce34103ac8cca153cd2fb169e
Sean Stacho <sstacho@sei.cmu.edu>
2024-11-05 11:35:53 -0500

    SDS-1764 - enhance ActionButton component to permit the use of either a button or a link

--- src/components/ActionButton/ActionButton.vue
+++ src/components/ActionButton/ActionButton.vue
@@ -1,68 +1,20 @@
 <template>
-  <button
+  <component
+    :is="href ? 'a' : 'button'"
     data-id="sds-action-button"
     :data-pending="pending || undefined"
-    :type="type"
+    :type="!href ? type : undefined"
+    :target="external ? '_blank' : undefined"
+    :rel="external ? 'noopener noreferrer' : undefined"
+    :href="href ?? undefined"
     :class="[btnClass, kindClass, variantClass, sizeClass, disabledClass, activeClass, blockClass, pendingClass]"
-    :disabled="disabled"
+    :disabled="!href ? disabled : undefined"
     :aria-disabled="disabled"
     @click="onClick"
   >
-    <span
-      v-if="pending"
-      class="relative flex items-center"
-    >
-      <span
-        class="absolute inset-0 flex items-center"
-        :class="{
-          'justify-center': !block
-        }"
-      >
-        <svg
-          class="animate-spin text-current"
-          :class="{
-            'h-5 w-5': size === 'lg',
-            'h-4 w-4': size !== 'lg' && size !== 'xs' && size !== 'sm',
-            'h-3.5 w-3.5': size === 'sm',
-            'h-3 w-3': size === 'xs'
-          }"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          role="graphics-symbol"
-        >
-          <title>Pending</title>
-          <circle
-            class="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            stroke-width="4"
-          />
-          <path
-            class="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          />
-        </svg>
-      </span>
-      <span
-        class="inline-flex items-center opacity-0"
-        :class="{
-          'gap-2': size !== 'xs',
-          'gap-1': size === 'xs'
-        }"
-      >
-        <!-- @slot Action Button content. -->
-        <slot />
-      </span>
-    </span>
-    <template v-else>
-      <!-- @slot Button content. -->
-      <slot />
-    </template>
-  </button>
+    <!-- @slot ActionButton content. -->
+    <slot />
+  </component>
 </template>
 
 <script setup lang="ts">
@@ -79,6 +31,20 @@ const props = defineProps({
    * Determines the color of the component.
    */
   variant: { type: String as PropType<'gray' | 'red' | 'blue'>, default: 'gray' },
+  /**
+   * Determines the HTML tag use for the component
+   */
+  href: {
+    type: String,
+    default: null
+  },
+  /**
+   * Applies the appropriate attributes for external links and opens them in a new tab. It also creates a REL attribute that prevents browser sniffing.
+   */
+   external: {
+    type: Boolean,
+    default: false
+  },
   /**
    * Determines the HTML type attribute for the button.
    */
