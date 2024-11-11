da1f78e7371ce04fb283a418b98b8c187414e06a
Sean Stacho <sstacho@sei.cmu.edu>
2024-11-11 13:52:47 -0500

    SDS-1754 - revise "addSnapshotSerializer" logic via setup/config file

--- setup.ts
+++ setup.ts
@@ -1,7 +1,10 @@
 import type { SnapshotSerializer } from 'vitest'
 import { vi, expect } from 'vitest'
 import { config } from '@vue/test-utils'
+import snapshotSerializer from 'jest-serializer-vue'
 import SeiDesignSystem from './src/components'
+
+const regex = new RegExp(/data-v-\w+="[^"]*"/g)
 
 config.global.plugins.push(SeiDesignSystem)
 
@@ -13,14 +16,24 @@ config.global.stubs = {
   transition: false
 }
 
+expect.addSnapshotSerializer(snapshotSerializer)
+
 /**
  * Remove all scoped data-v-* attributes via applicable components in order to match/test 
  * snapshots consistently between running scripts for both coverage and tests
  */
 expect.addSnapshotSerializer({
-  test: (val) => val,
-  serialize(val, _config, _indentation, _depth, _refs, _printer) {
-    const regex = new RegExp(/data-v-\w+="[^"]*"/g)
-    return regex.test(`${val.outerHTML}`) ? `${val.outerHTML}`.replace(regex, '') : `${val.outerHTML}`
+  test: (val) => {
+    return val && regex.test(val) || val && regex.test(`${val.outerHTML}`)
+  },
+  serialize(val, config, indentation, depth, refs, printer) {
+    const html = typeof val === 'object' ? `${val.outerHTML}`.replace(regex, '') : val.replace(regex, '')
+    return printer(
+      html,
+      config,
+      indentation,
+      depth,
+      refs
+    )
   }
 } satisfies SnapshotSerializer)
