f1a682c84c7f95cf443d21c0df133fed6a681a2f
dwtompkins <dwtompkins@sei.cmu.edu>
2024-11-20 16:57:43 -0500

    Updating Badge tests to create snapshots against all variants and all types. (SDS-1687)

--- src/components/Badge/Badge.spec.ts
+++ src/components/Badge/Badge.spec.ts
@@ -3,25 +3,41 @@ import { mount } from '@vue/test-utils'
 import Component from './Badge.vue'
 
 describe('Badge', () => {
+  /* Default badge snapshot. Tests slot content. */
   it('should match its default snapshot', () => {
     const wrapper = mount(Component, {
-      slots: {
-        default: 'Badge'
-      }
+      slots: { default: 'Badge' },
     })
-    expect(wrapper.element).toMatchSnapshot()
+    expect(wrapper.html()).toMatchSnapshot()
+    expect(wrapper.text()).toEqual('Badge')
   })
 
-  it('should match its variant primary, type light-border snapshot', () => {
-    const wrapper = mount(Component, {
-      slots: {
-        default: 'Badge'
-      },
-      props: {
-        variant: 'primary',
-        type: 'light-border'
-      }
+  /* All props */
+  const variants = ['gray', 'tan', 'yellow', 'orange', 'red', 'purple', 'indigo', 'blue', 'teal', 'green']
+  const types = ['light-border', 'light', 'medium', 'dark']
+
+  /* Loop through props and test against each one */
+  variants.forEach(variant => {
+    it(`should match its variant (${variant}) snapshot`, () => {
+      const badgeVariant = `Badge ${variant}`
+      const wrapper = mount(Component, {
+        slots: { default: badgeVariant },
+        props: { variant: variant }
+      })
+      expect(wrapper.html()).toMatchSnapshot()
+      expect(wrapper.text()).toEqual(badgeVariant)
     })
-    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  types.forEach(type => {
+    it(`should match its type (${type}) snapshot`, () => {
+      const badgeType = `Badge ${type}`
+      const wrapper = mount(Component, {
+        slots: { default: badgeType },
+        props: { type: type }
+      })
+      expect(wrapper.html()).toMatchSnapshot()
+      expect(wrapper.text()).toEqual(badgeType)
+    })
   })
 })
