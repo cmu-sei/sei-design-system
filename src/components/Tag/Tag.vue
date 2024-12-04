6fb4a8fbe2981836dcf614ee3b951dcd2eb5ca6b
Sean Stacho <sstacho@sei.cmu.edu>
2024-12-04 10:59:13 -0500

    SDS-1789 - revise component implementation with props

--- src/components/Tag/Tag.vue
+++ src/components/Tag/Tag.vue
@@ -4,13 +4,92 @@
     data-id="sds-tag"
     :class="[sizeClass]"
   >
-    <div class="flex flex-row">
-      <span>
-        <!-- @slot default/Label context. -->
-        <slot name="default">
+    <div class="flex flex-row flex-nowrap">
+      <!-- @slot Left slot content. -->
+      <slot name="leftSlot" />
+      <SdsLink 
+        v-if="href && !readonly"
+        kind="tertiary"
+        type="standalone"
+        :href="href"
+        :external="external"
+        :size="size"
+      >
+        <!-- @slot Label content. -->
+        <slot name="label">
+          {{ label }}
+        </slot>
+      </SdsLink>
+      <span v-else>
+        <!-- @slot Label content. -->
+        <slot name="label">
           {{ label }}
         </slot>
       </span>
+      <template v-if="action && !readonly">
+        <template v-if="action === 'add'">
+          <button 
+            ref="button" 
+            type="button"
+            class="text-blue-600 hover:bg-blue-50 rounded-full"
+          >
+            <SdsSvgIcon
+              aria-hidden="true"
+              fill="none"
+              preserveAspectRatio="xMidYMid meet"
+              role="img"
+              :height="icons.add.height"
+              :path="icons.add.path"
+              :width="icons.add.width"
+              :view-box="icons.add.viewBox"
+            />
+            <span class="sr-only">Add</span>
+          </button>
+        </template>
+        <template v-else-if="action === 'remove'">
+          <button 
+            ref="button" 
+            type="button"
+            class="text-gray-600 hover:bg-gray-50 rounded-full"
+          >
+            <SdsSvgIcon
+              aria-hidden="true"
+              fill="none"
+              preserveAspectRatio="xMidYMid meet"
+              role="img"
+              :height="icons.remove.height"
+              :path="icons.remove.path"
+              :width="icons.remove.width"
+              :view-box="icons.remove.viewBox"
+            />
+            <span class="sr-only">Remove</span>
+          </button>
+        </template>
+        <template v-else-if="action === 'destroy'">
+          <button 
+            ref="button" 
+            type="button"
+            class="text-red-600 hover:bg-red-50 rounded-full"
+          >
+            <SdsSvgIcon
+              aria-hidden="true"
+              fill="none"
+              preserveAspectRatio="xMidYMid meet"
+              role="img"
+              :height="icons.destroy.height"
+              :path="icons.destroy.path"
+              :width="icons.destroy.width"
+              :view-box="icons.destroy.viewBox"
+            />
+            <span class="sr-only">Destroy</span>
+          </button>
+        </template>
+      </template>
+      <!-- @slot Action slot content -->
+      <slot 
+        v-else 
+        name="action"
+      />
     </div>
   </div>
 </template>
@@ -27,37 +106,50 @@ defineOptions({
 
 const props = defineProps({
   /**
-   * Determines the type of content of the left slot.
+   * Determines the type of action or button.
    */
-  content: { type: String as PropType<'avatar' | 'chip' | 'icon' | 'indicator'>, default: null },
+  action: { type: String as PropType<'add' | 'remove' | 'destroy'>, default: null },
   /**
-   * Disables the component to prevent user interaction.
+   * Applies the appropriate attributes for external links and opens them in a new tab. It also creates a REL attribute that prevents browser sniffing.
    */
-  disabled: { type: Boolean, default: false },
+  external: { type: Boolean, default: false },
   /**
    * Provides the "href" link for the tag or label.
    */
   href: { type: String, default: null },
   /**
-   * Determines the purpose and particular function of the button or right slot.
-   */
-  kind: { type: String as PropType<'blue' | 'red' | 'gray'>, default: null },
-  /**
    * Determines the text of the label.
    */
   label: { type: String as PropType<string | null>, default: null },
   /**
-   * Determines the shape of the chip.
+   * Determines whether or not the tag is read-only.
    */
-  shape: { type: String as PropType<'circle' | 'square'>, default: 'square' },
+  readonly: { type: Boolean, default: false },
   /**
    * Determines the size of the tag.
    */
   size: { type: String as PropType<'sm' | 'md'>, default: 'sm' },
-  /**
-   * Determines the color of the chip.
-   */
-  variant: { type: String as PropType<'gray' | 'tan' | 'yellow' | 'orange' | 'red' | 'purple' | 'indigo' | 'blue' | 'teal' | 'green'>, default: 'gray' }
+})
+
+const icons = ref<Record<string, { height: number; path: string; viewBox: string; width: number; }>>({
+  add: {
+    height: 16,
+    path: 'M12.0625 8.25C12.0625 8.60156 11.7695 8.89453 11.4375 8.89453H8.625V11.707C8.625 12.0391 8.33203 12.3125 8 12.3125C7.64844 12.3125 7.375 12.0391 7.375 11.707V8.89453H4.5625C4.21094 8.89453 3.9375 8.60156 3.9375 8.25C3.9375 7.91797 4.21094 7.64453 4.5625 7.64453H7.375V4.83203C7.375 4.48047 7.64844 4.1875 8 4.1875C8.33203 4.1875 8.625 4.48047 8.625 4.83203V7.64453H11.4375C11.7695 7.625 12.0625 7.91797 12.0625 8.25Z',
+    viewBox: '0 0 16 16',
+    width: 16
+  },
+  remove: {
+    height: 16,
+    path: 'M11.4375 8.875H4.5625C4.21094 8.875 3.9375 8.60156 3.9375 8.25C3.9375 7.91797 4.21094 7.625 4.5625 7.625H11.4375C11.7695 7.625 12.0625 7.91797 12.0625 8.25C12.0625 8.60156 11.7695 8.875 11.4375 8.875Z',
+    viewBox: '0 0 16 16',
+    width: 16
+  },
+  destroy: {
+    height: 16,
+    path: 'M10.9297 10.3203C11.1836 10.5547 11.1836 10.9648 10.9297 11.1992C10.8125 11.3164 10.6562 11.375 10.5 11.375C10.3242 11.375 10.168 11.3164 10.0508 11.1992L8 9.14844L5.92969 11.1992C5.8125 11.3164 5.65625 11.375 5.5 11.375C5.32422 11.375 5.16797 11.3164 5.05078 11.1992C4.79688 10.9648 4.79688 10.5547 5.05078 10.3203L7.10156 8.25L5.05078 6.19922C4.79688 5.96484 4.79688 5.55469 5.05078 5.32031C5.28516 5.06641 5.69531 5.06641 5.92969 5.32031L8 7.37109L10.0508 5.32031C10.2852 5.06641 10.6953 5.06641 10.9297 5.32031C11.1836 5.55469 11.1836 5.96484 10.9297 6.19922L8.87891 8.26953L10.9297 10.3203Z',
+    viewBox: '0 0 16 16',
+    width: 16
+  }
 })
 
 const sizeClass = computed(() => {
