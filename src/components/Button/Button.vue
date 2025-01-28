3b498945a99427ed71e7a4f69474e12b26db3b5f
dwtompkins <dwtompkins@sei.cmu.edu>
2025-01-28 14:52:28 -0500

    Added in Plaid theme specific styling to the Button component.

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -15,10 +15,11 @@
     >
       <span class="absolute inset-0 flex items-center justify-center">
         <svg
+          data-id="spinner"
           class="animate-spin text-current"
           :class="{
             'h-5 w-5': size === 'lg',
-            'h-4 w-4': size !== 'lg' && size !== 'xs',
+            'h-4 w-4 sds-theme-plaid:size-6': size !== 'lg' && size !== 'xs',
             'h-3 w-3': size === 'xs'
           }"
           xmlns="http://www.w3.org/2000/svg"
@@ -28,7 +29,7 @@
         >
           <title>Pending</title>
           <circle
-            class="opacity-25"
+            class="opacity-25 fill-transparent"
             cx="12"
             cy="12"
             r="10"
@@ -50,6 +51,15 @@
     <template v-else>
       <!-- @slot Button content. -->
       <slot />
+      <svg
+        v-if="props.kind === 'primary' || props.kind === 'secondary'"
+        data-id="arrow"
+        class="hidden sds-theme-plaid:!flex w-[13px]"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 15 13"
+      >
+        <path d="M13.6875 7.71875L8.6875 12.7188C8.5 12.9062 8.25 13 8 13C7.71875 13 7.46875 12.9062 7.28125 12.7188C6.875 12.3438 6.875 11.6875 7.28125 11.3125L10.5625 8H1C0.4375 8 0 7.5625 0 7C0 6.46875 0.4375 6 1 6H10.5625L7.28125 2.71875C6.875 2.34375 6.875 1.6875 7.28125 1.3125C7.65625 0.90625 8.3125 0.90625 8.6875 1.3125L13.6875 6.3125C14.0938 6.6875 14.0938 7.34375 13.6875 7.71875Z" />
+      </svg>
     </template>
   </button>
 </template>
@@ -154,7 +164,7 @@ const pendingClass = computed(() => {
 })
 
 const blockClass = computed(() => {
-  return props.block ? 'btn-block' : ''
+  return props.block ? 'btn-block sds-theme-plaid:flex sds-theme-plaid:justify-center' : ''
 })
 
 const onClick = () => {
