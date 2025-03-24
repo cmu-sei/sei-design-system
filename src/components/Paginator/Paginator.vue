0ffbfd973857cf29e7edc2895cd0d274d5dc89ba
Sean Stacho <sstacho@sei.cmu.edu>
2025-03-24 13:00:07 -0400

    SDS-1772 - add loading spinner to pager button(s)

--- src/components/Paginator/Paginator.vue
+++ src/components/Paginator/Paginator.vue
@@ -53,7 +53,12 @@
           :disabled="page === currentPage || loading"
           :aria-disabled="page === currentPage || loading"
           :aria-current="page === currentPage ? 'page' : undefined"
-          :aria-label="page.toLocaleString() === '...' ? 'Jump to page &quot;x&quot;' : page === currentPage ? `Current page, page ${page}` : `Go to page ${page}` "
+          :aria-label="page === currentPage && loading ?
+            'Loading' :
+            page === currentPage ? 
+              `Current page, page ${page}` : 
+              `Go to page ${page}`
+          "
           class="
             flex items-center justify-center grow-1 shrink-1
             border rounded
@@ -63,10 +68,12 @@
             p-2
           "
           :class="{
+            'pointer-events-none': loading,
+            'bg-gray-600/20 border-gray-600/20': page === currentPage && loading,
             'bg-transparent hover:bg-gray-600/10 border-0': page === '...',
-            'bg-white hover:bg-gray-600/10 border-gray-600/20 ': page !== '...' && page !== currentPage && !loading,
             'bg-white border-gray-600/10 text-gray-600/10 dark:text-gray-400/10': page !== '...' && page !== currentPage && loading,
-            'bg-blue-50 dark:bg-blue-900 border-blue-600 dark:border-blue-400 shadow-inner shadow-blue-600/15': page === currentPage
+            'bg-white hover:bg-gray-600/10 border-gray-600/20': page !== '...' && page !== currentPage && !loading,
+            'bg-blue-50 dark:bg-blue-900 border-blue-600 dark:border-blue-400 shadow-inner shadow-blue-600/15': page === currentPage && !loading
           }"
           @click.prevent="goToPage(page, $event)"
         >
@@ -87,7 +94,39 @@
             :width="icons['ellipsis'].width"
           />
           <template v-else>
-            {{ page.toLocaleString() }}
+            <span 
+              v-if="page === currentPage && loading"
+              class="flex relative h-full w-full"
+            >
+              <span class="absolute inset-0 flex items-center justify-center">
+                <svg
+                  data-id="spinner"
+                  class="animate-spin text-current h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  role="graphics-symbol"
+                >
+                  <title>Loading</title>
+                  <circle
+                    class="opacity-25 fill-transparent"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    stroke-width="4"
+                  />
+                  <path
+                    class="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  />
+                </svg>
+              </span>
+            </span>
+            <template v-else>
+              {{ page.toLocaleString() }}
+            </template>
           </template>
         </button>
       </li>
