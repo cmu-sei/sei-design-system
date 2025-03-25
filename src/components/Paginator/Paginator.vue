f8dea412cb19bca695e81dc1c3a3122c574feca0
Sean Stacho <sstacho@sei.cmu.edu>
2025-03-25 12:41:54 -0400

    SDS-1772 - update paginator buttons with SdsLoadingSpinner component

--- src/components/Paginator/Paginator.vue
+++ src/components/Paginator/Paginator.vue
@@ -111,29 +111,10 @@
             class="flex relative h-full w-full"
           >
             <span class="absolute inset-0 flex items-center justify-center">
-              <svg
-                data-id="spinner"
-                class="animate-spin text-current text-gray-600 dark:text-gray-400 h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                role="graphics-symbol"
-              >
-                <title>Loading</title>
-                <circle
-                  class="opacity-25 fill-transparent"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  stroke-width="4"
-                />
-                <path
-                  class="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                />
-              </svg>
+              <SdsLoadingSpinner
+                size="sm"
+                class="text-gray-600 dark:text-gray-400"
+              />
             </span>
           </span>
           <template v-else>
