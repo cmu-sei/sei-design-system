e53fe3fa9f3d2a34adb506fe546462cffbfddcf9
Jason D Shimkoski <jdshimkoski@sei.cmu.edu>
2025-04-15 15:53:10 -0400

    Fix optional chaining for NuxtLink component resolution in Link.vue

--- src/components/Link/Link.vue
+++ src/components/Link/Link.vue
@@ -59,7 +59,7 @@ const props = defineProps({
 })
 
 const linkComponent = computed(() => {
-  if (props.to) return getCurrentInstance().appContext.components.NuxtLink || 'a'
+  if (props.to) return getCurrentInstance()?.appContext?.components?.NuxtLink || 'a'
   return 'a'
 })
 
