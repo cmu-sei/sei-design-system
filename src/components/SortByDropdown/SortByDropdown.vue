fed1ea7d9cb0dc7ae86ceeaff6c73eac422b1a64
Sean Stacho <sstacho@sei.cmu.edu>
2025-11-11 12:36:16 -0500

    feat(SortByDropdown): implement SortByDropdown component with props and template structure

--- src/components/SortByDropdown/SortByDropdown.vue
+++ src/components/SortByDropdown/SortByDropdown.vue
@@ -1,15 +1,141 @@
 <template>
-  <div data-id="sds-sort-by-dropdown" />
+  <SdsFloatingUi
+    data-id="sds-sort-by-dropdown"
+    :placement="placement"
+    :popper-class="`absolute border shadow-lg rounded-theme-md bg-white border-gray-200 dark:border-gray-700 dark:bg-gray-850 w-56 ${zIndexClass}`"
+    hide-arrow
+    placement-top-arrow-class="-bottom-1.5 border-t-0 border-l-0"
+    placement-right-arrow-class="-left-1.5 border-t-0 border-r-0"
+    placement-bottom-arrow-class="-top-1.5 border-b-0 border-r-0"
+    placement-left-arrow-class="-right-1.5 border-b-0 border-l-0"
+    shift
+  >
+    <template #trigger="{ isOpen, toggle }">
+      <SdsActionButton 
+        :id="id"
+        ref="button"
+        :kind="kind"
+        :variant="variant"
+        :size="size" 
+        :active="isOpen"
+        :disabled="disabled"
+        aria-haspopup="true"
+        :aria-expanded="isOpen"
+        @click="toggle()"
+      >
+        <!-- @slot Title content of trigger button. -->
+        <slot name="title">
+          <span>{{ title }}</span>
+        </slot>
+        <svg
+          class="inline-block self-center w-5 h-5 -mr-1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </SdsActionButton>
+    </template>
+  </SdsFloatingUi>
 </template>
 
 <script lang="ts" setup>
-export interface SortByDropdownOption {
+import type { RadioGroupOption, RadioGroupOptionValue } from '../RadioGroup/RadioGroup.vue'
+import SdsActionButton from '../ActionButton/ActionButton.vue'
+import SdsFloatingUi from '../FloatingUi/FloatingUi.vue'
+
+export interface SortByDropdownOption extends RadioGroupOption<RadioGroupOptionValue> {
   id: number | string;
+  type: SortByOptionType;
 }
 
 export type SortByDropdownPlacement = 'auto' | 'top' | 'right' | 'bottom-start'
+export type SortByOptionType = 'alpha' | 'chronological' | 'numerical' | 'custom'
+
+const SORT_BY_OPTION_TYPE = Object.freeze({
+  'ALPHA': {
+    'ASCENDING': 'A-Z',
+    'DESCENDING': 'Z-A'
+  },
+  'CHRONOLOGICAL': {
+    'ASCENDING': 'Oldest first',
+    'DESCENDING': 'Newest first'
+  },
+  'NUMERICAL': {
+    'ASCENDING': 'Lowest first',
+    'DESCENDING': 'Highest first'
+  },
+  'CUSTOM': {
+    'ASCENDING': (value: string) => {
+      return value ? `Most ${value} first` : 'Most first'
+    },
+    'DESCENDING': (value: string) => {
+      return value ? `Least ${value} first` : 'Least first'
+    }
+  }
+} as const)
 
 defineOptions({
   name: "SdsSortByDropdown"
+})
+
+const props = defineProps({
+  /**
+   * Determines the purpose and particular function of the component.
+   */
+  kind: { type: String as PropType<'primary' | 'secondary' | 'ghost'>, default: 'secondary' },
+  /**
+   * Determines the color of the component.
+   */
+  variant: { type: String as PropType<'gray' | 'blue'>, default: 'gray' },
+  /**
+   * Determines the size.
+   */
+  size: { type: String as PropType<'xs' | 'sm' | 'md' | 'lg'>, default: 'md' },
+  /**
+   * The z-index for the popover.
+   */
+  zIndex: { type: String as PropType<'0' | '10' | '20' | '30' | '40' | '50' | 'auto' | ''>, required: false, default: '50' },
+  /**
+   * The title for the toggle button.
+   */
+  title: { type: String, default: 'Sort by' },
+  /**
+   * Determines the placement of the dropdown on the screen.
+   */
+  placement: { type: String as PropType<SortByDropdownPlacement>, default: 'bottom-start' },
+  /**
+   * Determines if the dropdown is disabled
+   */
+  disabled: { type: Boolean, default: false }
+})
+
+const id = useId()
+const button = ref<HTMLButtonElement | undefined>()
+
+const zIndexClass = computed(() => {
+  switch (props.zIndex) {
+    case '0':
+      return 'z-0'
+    case '10':
+      return 'z-10'
+    case '20':
+      return 'z-20'
+    case '30':
+      return 'z-30'
+    case '40':
+      return 'z-40'
+    case '50':
+      return 'z-50'
+    case 'auto':
+      return 'z-auto'
+    default:
+      return ''
+  }
 })
 </script>
