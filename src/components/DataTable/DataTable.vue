ee7b7ef37c3cd735b8c521b84d38247aaf028294
Sean Stacho <sstacho@sei.cmu.edu>
2026-01-14 09:13:20 -0500

    feat: add DataTable component

---
+++ src/components/DataTable/DataTable.vue
@@ -0,0 +1,44 @@
+<template>
+  <div data-id="sds-data-table">
+    <SdsTable 
+      v-bind="{ ...$props.data, ...$attrs }"
+    >
+      <template
+        v-for="(_, name) in $slots"
+        #[name]="slotProps"
+      >
+        <slot
+          :name="name"
+          v-bind="slotProps ?? {}"
+        />
+      </template>
+    </SdsTable>
+    <div>
+      <SdsPaginator 
+        v-bind="{ ...$props.meta, ...$attrs }"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import SdsPaginator from '../Paginator/Paginator.vue'
+import SdsTable from '../Table/Table.vue'
+
+defineOptions({
+  name: "SdsDataTable"
+})
+
+defineProps({
+  data: {
+    type: Object,
+    required: false,
+    default: () => ({})
+  },
+  meta: {
+    type: Object,
+    required: false,
+    default: () => ({})
+  }
+})
+</script>
